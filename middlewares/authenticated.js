d6cfb4bda7c0dd29c7d26668f390b793d016f14a
Daniela Suarez <cindydanielasg@gmail.com>
2017-08-08 11:38:49 -0500

    Creación de middleware en carpeta middleware con nombre authenticated.js además se añadio a la ruta de usuarios en pruebas el middleware pasandolo como parametro

---
+++ middlewares/authenticated.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var jwt  = require('jwt-simple');
+var moment = require('moment');
+
+var secret = 'clave_secreta';
+
+exports.ensureAuth = function(req, res, next){
+    //Recoger la autorizacion a través de un header que se llama authorization
+
+    if (!req.headers.authorization) {
+        return res.status(403).send({message: 'La petición no tiene la cabecera de autenticación'});
+    }
+
+    var token = req.headers.authorization.replace(/['"]+/g, '');
+
+    try {
+        var payload = jwt.decode(token, secret);
+         if (payload.exp <= moment().unix()) {
+             return res.status(401).send({message: 'El token ha expirado, debe autenticarse de nuevo'});
+         }
+
+    } catch (ex) {
+        console.log(ex);
+        return res.status(404).send({message: 'Token no valido'});
+
+    }
+
+  req.user = payload;
+
+  next();
+
+};
