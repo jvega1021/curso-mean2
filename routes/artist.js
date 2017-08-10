1bcfe50aab31a6d5c51db7350668ff26d4ebd689
Daniela Suarez <cindydanielasg@gmail.com>
2017-08-10 07:06:17 -0500

    Creando el controlador y las rutas de artista

--- routes/artist.js
+++ routes/artist.js
@@ -7,5 +7,6 @@ var api = express.Router(); //Permite hacer las funciones get, post, put
 var md_auth = require('../middlewares/authenticated'); //Para que solo un usuario de prueba pueda ingresar a los metodos
 
 api.get('/get-artist', md_auth.ensureAuth, ArtistController.getArtist);
+api.post('/save-artist', md_auth.ensureAuth, ArtistController.saveArtist);
 
 module.exports = api;
